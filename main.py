8227c84acc252508af0b4dc2ee2cfd5930c06f71
davebecoding <d01001010@csu.fullerton.edu>
2022-04-16 22:20:11 -0700

    Building Project

--- main.py
+++ main.py
@@ -110,3 +110,17 @@ class AbstractCar:
 
 
 # stage for PLAYER-01
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (180, 200)
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
+
+
+# AI <- CPU-PLAYER
